12f7b19b5275000bcf75f9022c8e77ed39c9e114
Diego-Alejandro-Zambrano-Avellan <zambranoavellan@gmail.com>
2024-06-03 10:01:20 -0500

    dividir

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ function multiply() {
     var result = num1 * num2;
     document.getElementById("result").innerText = "Resultado: " + result;
 }
+function divide() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    if (num2 === 0) {
+        document.getElementById("result").innerText = "No se puede dividir por cero";
+    } else {
+        var result = num1 / num2;
+        document.getElementById("result").innerText = "Resultado: " + result;
+    }
+}
