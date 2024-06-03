023a149ef3e8ec197ba712514f08cf052e0cff2d
Diego-Alejandro-Zambrano-Avellan <zambranoavellan@gmail.com>
2024-06-03 09:59:24 -0500

    resta

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function sum() {
     var result = num1 + num2;
     document.getElementById("result").innerText = "Resultado: " + result;
 }
+function subtract() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("result").innerText = "Resultado: " + result;
+}
